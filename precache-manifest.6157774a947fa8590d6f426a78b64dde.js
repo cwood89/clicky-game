c89b4c97cbc148223c21b4747095005a09a7d7e1
woodtop <cwood89@mac.com>
2019-02-25 15:28:04 -0500

    Updates

---
+++ precache-manifest.6157774a947fa8590d6f426a78b64dde.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "469bf4db0fef788bd9e3edf9b73cd3bf",
+    "url": "/clicky-game/static/media/car6.469bf4db.png"
+  },
+  {
+    "revision": "e130427d4155844f0a94",
+    "url": "/clicky-game/static/css/main.bb356273.chunk.css"
+  },
+  {
+    "revision": "7985e359b3121cf81761",
+    "url": "/clicky-game/static/js/runtime~main.7985e359.js"
+  },
+  {
+    "revision": "adc26382cbb5d12b53e7",
+    "url": "/clicky-game/static/js/2.adc26382.chunk.js"
+  },
+  {
+    "revision": "ab3e4f00fcbc2d06649bf26de1f32477",
+    "url": "/clicky-game/static/media/car1.ab3e4f00.png"
+  },
+  {
+    "revision": "731223f13231333068208768aceab209",
+    "url": "/clicky-game/static/media/car2.731223f1.png"
+  },
+  {
+    "revision": "caaa833415141efa9178763db9e8b72a",
+    "url": "/clicky-game/static/media/car3.caaa8334.png"
+  },
+  {
+    "revision": "febee114a047e48292ad510b6c0873ee",
+    "url": "/clicky-game/static/media/car4.febee114.png"
+  },
+  {
+    "revision": "0a4719bbe026d343961ed88c19a95d2b",
+    "url": "/clicky-game/static/media/car5.0a4719bb.png"
+  },
+  {
+    "revision": "e130427d4155844f0a94",
+    "url": "/clicky-game/static/js/main.e130427d.chunk.js"
+  },
+  {
+    "revision": "a3948eedfa35b15625b3ae13452229d6",
+    "url": "/clicky-game/static/media/car7.a3948eed.png"
+  },
+  {
+    "revision": "28ef2dddd45c10c9ee711029f2c5698f",
+    "url": "/clicky-game/static/media/car8.28ef2ddd.png"
+  },
+  {
+    "revision": "249624229db474c5c4643500e2b75886",
+    "url": "/clicky-game/static/media/car9.24962422.png"
+  },
+  {
+    "revision": "299d63234c9a54d819639a4da7d238b6",
+    "url": "/clicky-game/static/media/car10.299d6323.png"
+  },
+  {
+    "revision": "fd27e9df4aeaf3aaa1ecb38b65543770",
+    "url": "/clicky-game/static/media/car11.fd27e9df.png"
+  },
+  {
+    "revision": "42f1ce45430f2d5f0de109be359f3a31",
+    "url": "/clicky-game/static/media/car12.42f1ce45.png"
+  },
+  {
+    "revision": "c2c3ee0659abea2689b340cb16592fb7",
+    "url": "/clicky-game/static/media/road.c2c3ee06.jpg"
+  },
+  {
+    "revision": "47013adf998e54028494211b755fef82",
+    "url": "/clicky-game/index.html"
+  }
+];
